3ddb1c284bdd134bf1403d330b92f6d6efe90efb
gdi2290 <github@gdi2290.com>
2016-01-24 20:01:59 -0800

    feat: init source

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+ MIT License http://www.opensource.org/licenses/mit-license.php
+ Author Patrick Stapleton @gdi2290
+ Shamelessly based on bundle-loader by Dan Abramov @dan_abramov
+ Which was
+ Shamelessly based on bundle-loader by Tobias Koppers @sokra
+ */
+
+ var path = require('path');
+
+module.exports = function () {};
+module.exports.pitch = function (remainingRequest) {
+  this.cacheable && this.cacheable();
+  var query = this.query.substring(1).split(','),
+    bundleName = query[1] || '';
+  var filename = path.basename(remainingRequest);
+  var name = path.basename(remainingRequest, path.extname(filename));
+
+  bundleName = bundleName.replace(/\[filename\]/g, filename).replace(/\[name\]/g, name);
+
+  if (!promiseLib) {
+    throw new Error('You need to specify your Promise library of choice, e.g. require("promise?bluebird!./file.js")');
+  }
+
+  var result = [
+    'module.exports = function (namespace) {\n',
+    '  return new Promise(function (resolve) {\n',
+    '    require.ensure([], function (require) {\n',
+    '      if (namespace) {\n',
+    '        resolve(require(', JSON.stringify('!!' + remainingRequest), ')[namespace]);\n',
+    '      } else {\n',
+    '        resolve(require(', JSON.stringify('!!' + remainingRequest), '));\n',
+    '      }\n',
+    '    }' + (bundleName && (', ' + JSON.stringify(bundleName))) + ');\n',
+    '  });\n',
+    '}'
+  ];
+
+  return result.join('');
+};
